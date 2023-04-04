41d33ff468ea9862df2752e3093fb1113d662ec4
k2y <marycarychin@gmail.com>
2023-04-05 01:53:49 +0900

    Fix:server/db.js 파일 api 노출로 인한 commit 취소 후 재업로드

--- src/db.js
+++ src/db.js
@@ -14,3 +14,20 @@ const handleError = (error) => console.log('❌ DB Error', error);
 
 db.on('error', handleError);
 db.once('open', handleOpen);
+/**
+ * YOUTUBE API 받아오는 코드
+ *
+ * 참조 : https://developers.google.com/youtube/iframe_api_reference?hl=ko
+ *
+ * IFRAME 관련 글
+ * https://jdh5202.tistory.com/450
+ *
+ *
+ *  YOUTUBE API 활용해서 인기 곡 가지고 오는 블로그
+ * https://han-py.tistory.com/432
+ *
+ *
+ * YOUTUBE oauth 2.0
+ * https://42kim.github.io/TIL/oauth/
+ *
+ */
